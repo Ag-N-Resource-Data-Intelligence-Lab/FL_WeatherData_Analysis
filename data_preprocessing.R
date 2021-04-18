b1a22a197ff2d36e736c2f8c5d8e8eed278fb929
Chi Zhang <garry571420670@gmail.com>
2021-04-18 12:17:18 -0400

    first

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -1,1 +1,14 @@
-test
+#install.packages('lubridate')
+#install.packages('dplyr')
+#install.packages('tidyr')
+#install.packages('magrittr')
+#install.packages('RcppRoll')
+#install.packages('wrapr')
+Libs=c('lubridate','dplyr','tidyr','magrittr','RcppRoll','wrapr')
+lapply(Libs,library, character.only = TRUE)
+
+DtF=read.csv('C:/Users/Chi Zhang/Desktop/Florida_hourly_NCDC.csv',
+             sep=',',header=T,stringsAsFactors = F) %>%
+  mutate(Time=ymd_hms(Time))
+
+
