c0336d07f29af7051187c7d9069e65599b870050
Chi Zhang <garry571420670@gmail.com>
2021-04-19 00:45:24 -0400

    NUM NOT ALL 60

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -9,7 +9,7 @@
 Libs=c('lubridate','dplyr','tidyr','magrittr','RcppRoll','wrapr','data.table')
 lapply(Libs,library, character.only = TRUE)
 
-DtF=read.csv('C:/Users/Chi Zhang/Desktop/JAX.csv', sep=',',header=T,stringsAsFactors = F) %>%
+DtF=read.csv('C:/Users/Chi Zhang/Desktop/JAX.csv', sep=',',header=T,stringsAsFactors = F)[,-1] %>%
   mutate(Time=ymd_hms(Time))
 
 DtF %>% 
@@ -17,24 +17,29 @@ DtF %>%
   mutate(TimeLag_min=as.numeric(Time-lag(Time),units='mins')) %>% 
   group_by(TimeLag_min) %>% 
   tally %>% 
-  rename(Num_lag=n)
+  rename(Num_lag=n)%>%
+  view()
 
-DtF %>%
-  ggplot()+
-  geom_point(aes(Time,Precip_mm))
+
 
 
 source('https://raw.githubusercontent.com/ZyuAFD/SWRE_General_R_Functions/master/src/Regulate%205%20min.R')
 interval= 60
 
-DtF1=Regular_Time(DtF,interval)
+DtF2=Regular_Time(DtF,interval)
+DtF2 = DtF2[rowSums(is.na(DtF2)) == 0,]
 
-DtF1 %>%
+DtF2 %>%
   arrange(Time) %>%
   mutate(TimeLag_min=as.numeric(Time-lag(Time),units='mins')) %>%
   group_by(TimeLag_min) %>%
   tally %>%
-  rename(Num_lag=n)
+  rename(Num_lag=n)%>%
+  view()
+
+DtF2 %>%
+  ggplot()+
+  geom_point(aes(Time,Precip_mm))
 
 
 
